76ddcdc4752460919edd30f9341cb4a278ab9ace
amandosneto <amandos@br.ibm.com>
2019-06-28 13:06:24 -0300

    Update BuscaTradicional.java

--- exemplos/lambda/BuscaTradicional.java
+++ exemplos/lambda/BuscaTradicional.java
@@ -7,13 +7,13 @@ import java.util.List;
 /**
  * 
  * @author Amando S. Neto
- * Vamos usar essa classe para resolver um problema usando uma express„o lamdba.
+ * Vamos usar essa classe para resolver um problema usando uma express√£o lamdba.
  */
 public class BuscaTradicional {
 	
 	/**
 	 * Verifica se a especie pode voar
-	 * @param animals ColeÁ„o de animais
+	 * @param animals Cole√ß√£o de animais
 	 * @param checkEspecie	Classe que verifica se voa.
 	 */
 	private static void print(List<Animal> animals, CheckEspecie checkEspecie) {
@@ -25,18 +25,36 @@ public class BuscaTradicional {
 		System.out.println();
 	}
 	
-	
+	/**
+	 * Verifica se a especie pode voar
+	 * @param animals Cole√ß√£o de animais
+	 * @param checkEspecie	Classe que verifica se voa.
+	 */
+	private static void print(List<Animal> animals, CheckEspecie checkEspecie) {
+		for (Animal animal : animals) {
+			if (checkEspecie.check(animal)) {
+				System.out.print(animal + " ");
+			}
+		}
+		System.out.println();
+	}
 	public static void main(String[] args) {
 		List<Animal> animals = new ArrayList<Animal>();
 		animals.add(new Animal("Peixe", true, false));
 		animals.add(new Animal("Galinha", false, false));
-		animals.add(new Animal("Gavi„o", false, true));
+		animals.add(new Animal("Gavi√£o", false, true));
 		animals.add(new Animal("Pato", true, true));
 		
 		print(animals, new VerificaSeVoa());
 		
 		//Problema: O que devo fazer para verificar se uma especie pode nadar, e as que podem nadar e voar
-		
+		//Problema: O que devo fazer para verificar se uma especie pode nadar, e as que podem nadar e voar
+		print2(animals, a -> a.especiePodeNadar());
+		System.out.println("============== Pode Nadar");
+		print2(animals, a -> a.especiePodeNadar() && a.especiePodeVoar());
+		System.out.println("============== Pode Nadar e Voar");
+		print2(animals, a -> a.especiePodeVoar());
+		System.out.println("============== Pode Voar");
 	}
 
 }
